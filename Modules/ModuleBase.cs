4d0f703d2a2222ea89c317346cd11778b4b64293
Piotr Gankiewicz <spetz90@gmail.com>
2016-10-18 13:56:11 +0200

    Init

---
+++ Modules/ModuleBase.cs
@@ -0,0 +1,11 @@
+﻿using Coolector.Services.Nancy;
+
+namespace Coolector.Services.Users.Modules
+{
+    public abstract class ModuleBase : ApiModuleBase
+    {
+        protected ModuleBase(string modulePath = "") : base(modulePath)
+        {
+        }
+    }
+}
---
+++ Queries/BrowseUsers.cs
@@ -0,0 +1,8 @@
+﻿using Coolector.Common.Types;
+
+namespace Coolector.Services.Users.Queries
+{
+    public class BrowseUsers : PagedQueryBase
+    {
+    }
+}
