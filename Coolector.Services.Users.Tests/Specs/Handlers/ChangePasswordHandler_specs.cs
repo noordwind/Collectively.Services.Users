9fb5e97b47984e81bf82f1b42b56603753873758
Grzegorz Paśnik <grzegorz.pasnik@noordwind.com>
2016-12-01 12:27:28 +0100

    change password handler unit tests

---
+++ Coolector.Services.Users.Tests/Specs/Handlers/ChangePasswordHandler_specs.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Coolector.Common.Commands;
+using Coolector.Common.Commands.Users;
+using Coolector.Common.Events.Users;
+using Coolector.Common.Services;
+using Coolector.Services.Users.Handlers;
+using Coolector.Services.Users.Services;
+using Machine.Specifications;
+using Moq;
+using RawRabbit;
+using RawRabbit.Configuration.Publish;
+using It = Machine.Specifications.It; 
+
+namespace Coolector.Services.Users.Tests.Specs.Handlers
+{
+    public abstract class ChangePasswordHandler_specs
+    {
+        protected static ChangePasswordHandler ChangePasswordHandler;
+        protected static IHandler Handler;
+        protected static Mock<IBusClient> BusClientMock;
+        protected static Mock<IPasswordService> PasswordServiceMock;
+
+        protected static ChangePassword Command;
+
+        protected static void Initialize()
+        {
+            Handler = new Handler();
+            BusClientMock = new Mock<IBusClient>();
+            PasswordServiceMock = new Mock<IPasswordService>();
+
+            ChangePasswordHandler = new ChangePasswordHandler(Handler, 
+                BusClientMock.Object, PasswordServiceMock.Object);
+
+            Command = new ChangePassword
+            {
+                CurrentPassword = "current",
+                NewPassword = "new",
+                UserId = "userId",
+                Request = new Request
+                {
+                    Id = Guid.NewGuid(),
+                    CreatedAt = DateTime.Now,
+                    Culture = "en-US",
+                    Name = "name",
+                    Origin = "coolector",
+                    Resource = "resource"
+                }
+            };
+        }
+    }
+
+    [Subject("ChangePasswordHandler HandleAsync")]
+    public class When_handle_async : ChangePasswordHandler_specs
+    {
+        Establish context = () => Initialize();
+
+        Because of = () => ChangePasswordHandler.HandleAsync(Command).Await();
+
+        It should_call_change_async = () => PasswordServiceMock.Verify(x => x.ChangeAsync(
+            Command.UserId,
+            Command.CurrentPassword,
+            Command.NewPassword), Times.Once);
+
+        It should_publish_password_changed_event = () => BusClientMock.Verify(x => x.PublishAsync(
+            Moq.It.Is<PasswordChanged>(e => e.RequestId == Command.Request.Id
+                                            && e.UserId == Command.UserId),
+            Moq.It.IsAny<Guid>(),
+            Moq.It.IsAny<Action<IPublishConfigurationBuilder>>()), Times.Once);
+
+
+        It should_not_publish_change_password_rejected_event = () => BusClientMock.Verify(x => x.PublishAsync(
+            Moq.It.IsAny<ChangePasswordRejected>(),
+            Moq.It.IsAny<Guid>(),
+            Moq.It.IsAny<Action<IPublishConfigurationBuilder>>()), Times.Never);
+    }
+}
--- Coolector.Services.Users/Handlers/ChangePasswordHandler.cs
+++ Coolector.Services.Users/Handlers/ChangePasswordHandler.cs
@@ -5,6 +5,7 @@ using Coolector.Common.Commands;
 using Coolector.Common.Commands.Users;
 using Coolector.Common.Domain;
 using Coolector.Common.Events.Users;
+using Coolector.Common.Services;
 using Coolector.Services.Users.Services;
 using RawRabbit;
 
@@ -12,33 +13,36 @@ namespace Coolector.Services.Users.Handlers
 {
     public class ChangePasswordHandler : ICommandHandler<ChangePassword>
     {
+        private readonly IHandler _handler;
         private readonly IBusClient _bus;
         private readonly IPasswordService _passwordService;
 
-        public ChangePasswordHandler(IBusClient bus, IPasswordService passwordService)
+        public ChangePasswordHandler(IHandler handler,
+            IBusClient bus, 
+            IPasswordService passwordService)
         {
+            _handler = handler;
             _bus = bus;
             _passwordService = passwordService;
         }
 
         public async Task HandleAsync(ChangePassword command)
         {
-            try
-            {
-                await _passwordService.ChangeAsync(command.UserId, command.CurrentPassword,
-                    command.NewPassword);
-                await _bus.PublishAsync(new PasswordChanged(command.Request.Id, command.UserId));
-            }
-            catch (ServiceException exception)
-            {
-                await _bus.PublishAsync(new ChangePasswordRejected(command.Request.Id, command.UserId,
-                    OperationCodes.InvalidPassword, exception.Message));
-            }
-            catch (Exception exception)
-            {
-                await _bus.PublishAsync(new ChangePasswordRejected(command.Request.Id, command.UserId,
-                    OperationCodes.Error, "Error when trying to change password."));
-            }
+            await _handler
+                .Run(async () => await _passwordService.ChangeAsync(command.UserId, 
+                    command.CurrentPassword, command.NewPassword))
+                .OnSuccess(async () => await _bus.PublishAsync(
+                    new PasswordChanged(command.Request.Id, command.UserId)))
+                .OnCustomError(async ex => await _bus.PublishAsync(
+                    new ChangePasswordRejected(command.Request.Id, command.UserId,
+                        OperationCodes.InvalidPassword, ex.Message)))
+                .OnError(async (ex, logger) =>
+                {
+                    logger.Error(ex, "Error when trying to change password.");
+                    await _bus.PublishAsync(new ChangePasswordRejected(command.Request.Id, command.UserId,
+                        OperationCodes.Error, "Error when trying to change password."));
+                })
+                .ExecuteAsync();
         }
     }
 }
