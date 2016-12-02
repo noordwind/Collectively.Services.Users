ccf1223bbe91b7b91225e7f39d64ca17da4139de
Grzegorz Paśnik <grzegorz.pasnik@noordwind.com>
2016-12-02 10:19:45 +0100

    set new password handler refactored, unit tests added

---
+++ Coolector.Services.Users.Tests/Specs/Handlers/SetNewPasswordHandler_specs.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using Coolector.Common.Commands;
+using Coolector.Common.Commands.Users;
+using Coolector.Common.Domain;
+using Coolector.Common.Events.Users;
+using Coolector.Common.Services;
+using Coolector.Services.Users.Handlers;
+using Coolector.Services.Users.Services;
+using Machine.Specifications;
+using Moq;
+using RawRabbit;
+using RawRabbit.Configuration.Publish;
+using It = Machine.Specifications.It;
+
+namespace Coolector.Services.Users.Tests.Specs.Handlers
+{
+    public class SetNewPasswordHandler_specs
+    {
+        protected static SetNewPasswordHandler SetNewPasswordHandler;
+        protected static IHandler Handler;
+        protected static Mock<IBusClient> BusClientMock;
+        protected static Mock<IPasswordService> PasswordServiceMock;
+
+        protected static SetNewPassword Command;
+
+        protected static void Initialize()
+        {
+            Handler = new Handler();
+            BusClientMock = new Mock<IBusClient>();
+            PasswordServiceMock = new Mock<IPasswordService>();
+            
+            SetNewPasswordHandler = new SetNewPasswordHandler(Handler, 
+                BusClientMock.Object, PasswordServiceMock.Object);
+
+            Command = new SetNewPassword
+            {
+                Request = new Request
+                {
+                    Id = Guid.NewGuid(),
+                    CreatedAt = DateTime.Now,
+                    Culture = "en-US",
+                    Name = "name",
+                    Origin = "coolector",
+                    Resource = "resource"
+                },
+                Email = "email",
+                Password = "password",
+                Token = "token"
+            };
+        }
+    }
+
+    [Subject("SetNewPasswordHandler HandleAsync")]
+    public class When_handle_async_set_new_password : SetNewPasswordHandler_specs
+    {
+        Establish context = () => Initialize();
+
+        Because of = () => SetNewPasswordHandler.HandleAsync(Command).Await();
+
+        It should_call_set_new_async = () => PasswordServiceMock.Verify(x => x.SetNewAsync(
+            Command.Email, Command.Token, Command.Password), Times.Once);
+
+        It should_publish_new_password_set_event = () => BusClientMock.Verify(x => x.PublishAsync(
+                Moq.It.Is<NewPasswordSet>(m => m.RequestId == Command.Request.Id
+                                               && m.Email == Command.Email),
+                Moq.It.IsAny<Guid>(),
+                Moq.It.IsAny<Action<IPublishConfigurationBuilder>>()),
+            Times.Once);
+
+        It should_not_publish_set_new_password_rejected = () => BusClientMock.Verify(x => x.PublishAsync(
+                Moq.It.IsAny<SetNewPasswordRejected>(),
+                Moq.It.IsAny<Guid>(),
+                Moq.It.IsAny<Action<IPublishConfigurationBuilder>>()),
+            Times.Never);
+    }
+
+
+    [Subject("SetNewPasswordHandler HandleAsync")]
+    public class When_handle_async_set_new_password_and_it_throws_custom_error : SetNewPasswordHandler_specs
+    {
+        protected static string ErrorCode = "Error";
+
+        Establish context = () =>
+        {
+            Initialize();
+            PasswordServiceMock.Setup(x => x.SetNewAsync(
+                    Moq.It.IsAny<string>(), Moq.It.IsAny<string>(), Moq.It.IsAny<string>()))
+                .Throws(new ServiceException(ErrorCode));
+        };
+
+        Because of = () => SetNewPasswordHandler.HandleAsync(Command).Await();
+
+        It should_call_set_new_async = () => PasswordServiceMock.Verify(x => x.SetNewAsync(
+            Command.Email, Command.Token, Command.Password), Times.Once);
+
+        It should_not_publish_new_password_set_event = () => BusClientMock.Verify(x => x.PublishAsync(
+                Moq.It.IsAny<NewPasswordSet>(),
+                Moq.It.IsAny<Guid>(),
+                Moq.It.IsAny<Action<IPublishConfigurationBuilder>>()),
+            Times.Never);
+
+        It should_publish_set_new_password_rejected = () => BusClientMock.Verify(x => x.PublishAsync(
+                Moq.It.Is<SetNewPasswordRejected>(m => m.RequestId == Command.Request.Id
+                                                       && m.Code == ErrorCode
+                                                       && m.Email == Command.Email),
+                Moq.It.IsAny<Guid>(),
+                Moq.It.IsAny<Action<IPublishConfigurationBuilder>>()),
+            Times.Once);
+    }
+
+    [Subject("SetNewPasswordHandler HandleAsync")]
+    public class When_handle_async_set_new_password_and_it_fails : SetNewPasswordHandler_specs
+    {
+        Establish context = () =>
+        {
+            Initialize();
+            PasswordServiceMock.Setup(x => x.SetNewAsync(
+                    Moq.It.IsAny<string>(), Moq.It.IsAny<string>(), Moq.It.IsAny<string>()))
+                .Throws<Exception>();
+        };
+
+        Because of = () => SetNewPasswordHandler.HandleAsync(Command).Await();
+
+        It should_call_set_new_async = () => PasswordServiceMock.Verify(x => x.SetNewAsync(
+            Command.Email, Command.Token, Command.Password), Times.Once);
+
+        It should_not_publish_new_password_set_event = () => BusClientMock.Verify(x => x.PublishAsync(
+                Moq.It.IsAny<NewPasswordSet>(),
+                Moq.It.IsAny<Guid>(),
+                Moq.It.IsAny<Action<IPublishConfigurationBuilder>>()),
+            Times.Never);
+
+        It should_publish_set_new_password_rejected = () => BusClientMock.Verify(x => x.PublishAsync(
+                Moq.It.Is<SetNewPasswordRejected>(m => m.RequestId == Command.Request.Id
+                                                       && m.Code == OperationCodes.Error
+                                                       && m.Email == Command.Email),
+                Moq.It.IsAny<Guid>(),
+                Moq.It.IsAny<Action<IPublishConfigurationBuilder>>()),
+            Times.Once);
+    }
+}
--- Coolector.Services.Users/Handlers/SetNewPasswordHandler.cs
+++ Coolector.Services.Users/Handlers/SetNewPasswordHandler.cs
@@ -5,6 +5,7 @@ using Coolector.Common.Commands;
 using Coolector.Common.Commands.Users;
 using Coolector.Common.Domain;
 using Coolector.Common.Events.Users;
+using Coolector.Common.Services;
 using Coolector.Services.Users.Services;
 using RawRabbit;
 
@@ -12,32 +13,33 @@ namespace Coolector.Services.Users.Handlers
 {
     public class SetNewPasswordHandler : ICommandHandler<SetNewPassword>
     {
+        private readonly IHandler _handler;
         private readonly IBusClient _bus;
         private readonly IPasswordService _passwordService;
 
-        public SetNewPasswordHandler(IBusClient bus, IPasswordService passwordService)
+        public SetNewPasswordHandler(IHandler handler, 
+            IBusClient bus, 
+            IPasswordService passwordService)
         {
+            _handler = handler;
             _bus = bus;
             _passwordService = passwordService;
         }
 
         public async Task HandleAsync(SetNewPassword command)
         {
-            try
-            {
-                await _passwordService.SetNewAsync(command.Email, command.Token, command.Password);
-                await _bus.PublishAsync(new NewPasswordSet(command.Request.Id, command.Email));
-            }
-            catch (ServiceException exception)
-            {
-                await _bus.PublishAsync(new SetNewPasswordRejected(command.Request.Id,
-                    OperationCodes.InvalidEmail, exception.Message, command.Email));
-            }
-            catch (Exception exception)
-            {
-                await _bus.PublishAsync(new SetNewPasswordRejected(command.Request.Id,
-                    OperationCodes.InvalidEmail, "Error when trying to set new password.", command.Email));
-            }
+            await _handler
+                .Run(async () => await _passwordService.SetNewAsync(command.Email, command.Token, command.Password))
+                .OnSuccess(async () => await _bus.PublishAsync(new NewPasswordSet(command.Request.Id, command.Email)))
+                .OnCustomError(async ex => await _bus.PublishAsync(new SetNewPasswordRejected(command.Request.Id,
+                    ex.Code, ex.Message, command.Email)))
+                .OnError(async (ex, logger) =>
+                {
+                    logger.Error(ex, "Error when trying to set new password.");
+                    await _bus.PublishAsync(new SetNewPasswordRejected(command.Request.Id,
+                        OperationCodes.Error, "Error when trying to set new password.", command.Email));
+                })
+                .ExecuteAsync();
         }
     }
 }
