4a0678873ee7ed68ef5127ae21520a7f18b54403
Grzegorz Paśnik <grzegorz.pasnik@noordwind.com>
2016-12-02 08:46:49 +0100

    post message on facebook wall refactored, unit tests added

---
+++ Coolector.Services.Users.Tests/Specs/Handlers/PostMessageOnFacebookWall_specs.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Coolector.Common.Commands;
+using Coolector.Common.Commands.Facebook;
+using Coolector.Common.Events.Facebook;
+using Coolector.Common.Services;
+using Coolector.Services.Users.Handlers;
+using Coolector.Services.Users.Services;
+using Machine.Specifications;
+using Moq;
+using RawRabbit;
+using RawRabbit.Configuration.Publish;
+using It = Machine.Specifications.It;
+
+namespace Coolector.Services.Users.Tests.Specs.Handlers
+{
+    public abstract class PostMessageOnFacebookWall_specs
+    {
+        protected static PostMessageOnFacebookWallHandler PostMessageOnFacebookWallHandler;
+        protected static IHandler Handler;
+        protected static Mock<IBusClient> BusClientMock;
+        protected static Mock<IFacebookService> FacebookServiceMock;
+        protected static PostMessageOnFacebookWall Command;
+
+        protected static void Initialize()
+        {
+            Handler = new Handler();
+            BusClientMock = new Mock<IBusClient>();
+            FacebookServiceMock = new Mock<IFacebookService>();
+            PostMessageOnFacebookWallHandler = new PostMessageOnFacebookWallHandler(Handler,
+                BusClientMock.Object, FacebookServiceMock.Object);
+
+            Command = new PostMessageOnFacebookWall
+            {
+                AccessToken = "token",
+                Message = "message",
+                UserId = "userId",
+                Request = new Request
+                {
+                    Id = Guid.NewGuid(),
+                    CreatedAt = DateTime.Now,
+                    Culture = "en-US",
+                    Name = "name",
+                    Origin = "coolector",
+                    Resource = "resource"
+                }
+            };
+        }
+    }
+
+    [Subject("PostMessageOnFacebookWallHandler HandleAsync")]
+    public class When_handle_async_post_message_on_facebook : PostMessageOnFacebookWall_specs
+    {
+        Establish context = () => Initialize();
+
+        Because of = () => PostMessageOnFacebookWallHandler.HandleAsync(Command).Await();
+
+        It should_call_post_on_wall_async = () => FacebookServiceMock.Verify(x => x.PostOnWallAsync(
+                Command.AccessToken, Command.Message),
+            Times.Once);
+
+        It should_publish_message_posted_event = () => BusClientMock.Verify(x => x.PublishAsync(
+                Moq.It.Is<MessageOnFacebookWallPosted>(m => m.RequestId == Command.Request.Id
+                                                            && m.UserId == Command.UserId
+                                                            && m.Message == Command.Message),
+                Moq.It.IsAny<Guid>(),
+                Moq.It.IsAny<Action<IPublishConfigurationBuilder>>()),
+            Times.Once);
+
+        It should_not_publish_post_message_rejected = () => BusClientMock.Verify(x => x.PublishAsync(
+                Moq.It.IsAny<PostMessageOnFacebookWallRejected>(),
+                Moq.It.IsAny<Guid>(),
+                Moq.It.IsAny<Action<IPublishConfigurationBuilder>>()),
+            Times.Never);
+
+    }
+
+    [Subject("PostMessageOnFacebookWallHandler HandleAsync")]
+    public class When_handle_async_post_message_on_facebook_and_it_fails : PostMessageOnFacebookWall_specs
+    {
+        Establish context = () =>
+        {
+            Initialize();
+            FacebookServiceMock.Setup(x => x.PostOnWallAsync(Moq.It.IsAny<string>(), Moq.It.IsAny<string>()))
+                .Throws<Exception>();
+        };
+
+        Because of = () => PostMessageOnFacebookWallHandler.HandleAsync(Command).Await();
+
+        It should_call_post_on_wall_async = () => FacebookServiceMock.Verify(x => x.PostOnWallAsync(
+                Command.AccessToken, Command.Message),
+            Times.Once);
+
+        It should_publish_message_posted_event = () => BusClientMock.Verify(x => x.PublishAsync(
+                Moq.It.IsAny<MessageOnFacebookWallPosted>(),
+                Moq.It.IsAny<Guid>(),
+                Moq.It.IsAny<Action<IPublishConfigurationBuilder>>()),
+            Times.Never);
+
+        It should_not_publish_post_message_rejected = () => BusClientMock.Verify(x => x.PublishAsync(
+                Moq.It.Is<PostMessageOnFacebookWallRejected>(m => m.RequestId == Command.Request.Id
+                                                                  && m.UserId == Command.UserId
+                                                                  && m.Code == OperationCodes.Error
+                                                                  && m.Message == Command.Message),
+                Moq.It.IsAny<Guid>(),
+                Moq.It.IsAny<Action<IPublishConfigurationBuilder>>()),
+            Times.Once);
+
+    }
+}
--- Coolector.Services.Users/Handlers/PostMessageOnFacebookWallHandler.cs
+++ Coolector.Services.Users/Handlers/PostMessageOnFacebookWallHandler.cs
@@ -4,6 +4,7 @@ using Coolector.Common;
 using Coolector.Common.Commands;
 using Coolector.Common.Commands.Facebook;
 using Coolector.Common.Events.Facebook;
+using Coolector.Common.Services;
 using Coolector.Services.Users.Services;
 using RawRabbit;
 
@@ -11,31 +12,32 @@ namespace Coolector.Services.Users.Handlers
 {
     public class PostMessageOnFacebookWallHandler : ICommandHandler<PostMessageOnFacebookWall>
     {
+        private readonly IHandler _handler;
         private readonly IBusClient _bus;
-        private readonly IUserService _userService;
         private readonly IFacebookService _facebookService;
 
-        public PostMessageOnFacebookWallHandler(IBusClient bus, IUserService userService,
+        public PostMessageOnFacebookWallHandler(IHandler handler,
+            IBusClient bus,
             IFacebookService facebookService)
         {
+            _handler = handler;
             _bus = bus;
-            _userService = userService;
             _facebookService = facebookService;
         }
 
         public async Task HandleAsync(PostMessageOnFacebookWall command)
         {
-            try
-            {
-                await _facebookService.PostOnWallAsync(command.AccessToken, command.Message);
-                await _bus.PublishAsync(new MessageOnFacebookWallPosted(command.Request.Id,
-                    command.UserId, command.Message));
-            }
-            catch (Exception exception)
-            {
-                await _bus.PublishAsync(new PostMessageOnFacebookWallRejected(command.Request.Id,
-                    command.UserId, OperationCodes.Error, exception.Message, command.Message));
-            }
+            await _handler
+                .Run(async () => await _facebookService.PostOnWallAsync(command.AccessToken, command.Message))
+                .OnSuccess(async () => await _bus.PublishAsync(new MessageOnFacebookWallPosted(command.Request.Id,
+                    command.UserId, command.Message)))
+                .OnError(async (ex, logger) =>
+                {
+                    logger.Error(ex, "Error occured while posting message on facebook wall");
+                    await _bus.PublishAsync(new PostMessageOnFacebookWallRejected(command.Request.Id,
+                        command.UserId, OperationCodes.Error, ex.Message, command.Message));
+                })
+                .ExecuteAsync();
         }
     }
 }
