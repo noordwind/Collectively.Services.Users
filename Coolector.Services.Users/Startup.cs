bd911f2af1bea9fab38097421d2c6ab01c920dc9
Grzegorz Paśnik <grzegorz.pasnik@noordwind.com>
2017-01-26 13:57:01 +0100

    lockbox for dev env

--- Coolector.Services.Users/Startup.cs
+++ Coolector.Services.Users/Startup.cs
@@ -30,7 +30,7 @@ namespace Coolector.Services.Users
                 .AddEnvironmentVariables()
                 .SetBasePath(env.ContentRootPath);
 
-            if (env.IsProduction())
+            if (env.IsProduction() || env.IsDevelopment())
             {
                 builder.AddLockbox();
             }
