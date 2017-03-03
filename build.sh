2b6bcc53f8ac7cf78e1fd35d391f345175ef340b
spetz <spetz90@gmail.com>
2017-03-03 10:53:35 +0100

    Remove MyGet build

--- build.sh
+++
@@ -1,2 +0,0 @@
-dotnet restore --source "https://api.nuget.org/v3/index.json" --source "https://www.myget.org/F/collectively/api/v3/index.json" --no-cache
-dotnet pack "Collectively.Services.Users.Shared" -o .
