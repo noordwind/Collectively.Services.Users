f1cbc40700cebe89cc3a56f90936db5ed18a10db
Piotr Gankiewicz <spetz90@gmail.com>
2017-07-13 16:15:53 +0200

    Structure fix

--- run-tests.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-dotnet test Collectively.Services.Users.Tests/Collectively.Services.Users.Tests.csproj
---
+++ scripts/run-tests-e2e.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd tests/Collectively.Services.Users.Tests.EndToEnd
+dotnet test
