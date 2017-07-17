81610cbde071d11bd3c3589b5cef3badef631362
Piotr Gankiewicz <spetz90@gmail.com>
2017-07-17 13:42:30 +0200

    Updated ports and readme

--- src/Collectively.Services.Users/Program.cs
+++ src/Collectively.Services.Users/Program.cs
@@ -9,7 +9,7 @@ namespace Collectively.Services.Users
         public static void Main(string[] args)
         {
             WebServiceHost
-                .Create<Startup>()
+                .Create<Startup>(args: args)
                 .UseAutofac(Bootstrapper.LifetimeScope)
                 .UseRabbitMq(queueName: typeof(Program).Namespace)
                 .SubscribeToCommand<SignUp>()
