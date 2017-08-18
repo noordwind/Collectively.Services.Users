a662ad88b9a79f1c14d50ac164d0485272adcd3b
Piotr Gankiewicz <spetz90@gmail.com>
2017-08-18 21:15:42 +0200

    Owner role

--- src/Collectively.Services.Users/Domain/Roles.cs
+++ src/Collectively.Services.Users/Domain/Roles.cs
@@ -6,9 +6,11 @@
         public static string Associate => "associate";
         public static string Moderator => "moderator";
         public static string Administrator => "administrator";
+        public static string Owner => "owner";
 
         public static bool IsValid(string role)
         => role == User || role == Associate || 
-           role == Moderator || role == Administrator;
+           role == Moderator || role == Administrator ||
+           role == Owner;
     }
 }
